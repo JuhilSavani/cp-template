35038bd0ffa94a4c4e3a5a6928be85c595e0329b
JuhilSavani <savanijuhil@gmail.com>
2024-10-03 19:15:17 +0530

    Add new template

---
+++ minimalist.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// ---
+typedef long long ll;
+typedef unsigned long long ull;
+// --- 
+#define ff first
+#define ss second
+// ---
+#define endl '\n'
+#define newl cout << '\n'
+// ---
+const int MN = INT_MIN;
+const int MX = INT_MAX;
+const long long LL_MN = LLONG_MIN;
+const long long LL_MX = LLONG_MAX;
+// ---
+
+class Solver{
+  public:
+    void fastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+
+    // int cs=1;
+    void solve(){
+      // cerr << cs << ": ";
+
+      // cerr << endl; 
+      // ++cs;
+    }
+};
+
+int main(){
+  Solver solver;
+  solver.fastIO();
+
+  int tc; cin >> tc;
+  while(tc--){
+    solver.solve();
+  }
+
+  return 0;
+} // 青の彼方 (Ao no Kanata)
+
+
+
